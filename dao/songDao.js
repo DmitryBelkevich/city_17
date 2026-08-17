a63ad403705d5ab85d63113aeeb10741d0bed632
DmitryBelkevich <harleydav@mail.ru>
2026-08-17 06:43:05 +0300

    Create songDao.js

---
+++ dao/songDao.js
@@ -0,0 +1,13 @@
+class SongDao {
+  async getById(id) {
+    const song = new Song();
+
+      song.id = id;
+      song.title = "Воспоминание о былой любви";
+      song.text = "link";
+      song.score = "link";
+      song.playback = "link";
+    
+    return song;
+  }
+}
