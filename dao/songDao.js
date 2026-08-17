de80c917ec5ca21a6b90b838d5320ea114b08255
DmitryBelkevich <harleydav@mail.ru>
2026-08-17 08:14:49 +0300

    Update songDao.js

--- dao/songDao.js
+++ dao/songDao.js
@@ -1,11 +1,11 @@
-const database = [
-  { id: 1, band: "Король и Шут", title: "Воспоминание о былой любви" },
+const songs = [
+  { id: 1, band: "Король и Шут", title: "Воспоминание о былой любви", text: "", score: "", playback: "" },
   { id: 2, band: "Кино", title: "Группа крови" }
 ];
 
 class SongDao {
   getById(id) {
-    const song = database.find(song => song.id === id)
+    const song = songs.find(song => song.id === id)
     
     return song;
   }
