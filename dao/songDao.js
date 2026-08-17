7b165c50653ecd1e7e2cf0fa92b5c504db83de12
DmitryBelkevich <harleydav@mail.ru>
2026-08-17 08:15:38 +0300

    Update songDao.js

--- dao/songDao.js
+++ dao/songDao.js
@@ -1,6 +1,6 @@
 const songs = [
-  { id: 1, band: "Король и Шут", title: "Воспоминание о былой любви", text: "", score: "", playback: "" },
-  { id: 2, band: "Кино", title: "Группа крови" }
+  { id: 1, band: "Король и Шут", title: "Воспоминание о былой любви", text: "link1", score: "link2", playback: "link3" },
+  { id: 2, band: "Кино", title: "Группа крови", text: "link1", score: "link2", playback: "link3" }
 ];
 
 class SongDao {
