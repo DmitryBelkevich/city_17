7c065fc87f27244bd7eebaff3903acb32a5ecce3
DmitryBelkevich <harleydav@mail.ru>
2026-08-17 20:00:19 +0300

    Update song.js

--- models/song.js
+++ models/song.js
@@ -1,7 +1,40 @@
 class Song {
-  id;
-  title;
-  text;
-  score;
-  playback;
+  get id() {
+    return this.id;
+  }
+  set id(id) {
+    this.id = id;
+  }
+
+  get title() {
+    return this.title;
+  }
+
+  set title(title) {
+    this.id = id;
+  }
+
+  get text() {
+    return this.text;
+  }
+
+  set text(text) {
+    this.text = text;
+  }
+
+  get score() {
+    return this.score;
+  }
+
+  set score(score) {
+    this.score = score;
+  }
+
+  get playback() {
+    return this.playback;
+  }
+
+  set playback(playback) {
+    this.playback = playback;
+  }
 }
