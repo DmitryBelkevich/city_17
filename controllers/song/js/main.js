03031845a425e018b8d3899de195b6bf5a3c7da5
DmitryBelkevich <harleydav@mail.ru>
2026-08-18 09:28:01 +0300

    Update main.js

--- controllers/song/js/main.js
+++ controllers/song/js/main.js
@@ -1,20 +1,12 @@
 import Song from '../../../models/Song.js';
-import Musician from '../../../models/Musician.js';
 
 import SongDao from '../../../dao/SongDao.js';
 
-// ---
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-
 const id = urlParams.get('id');
 
-// ---
-const musician = new Musician();
-
-musician.voice = urlParams.get('voice');
-musician.instrument = urlParams.get('instrument');
-// ---
 // DAO
 const songDao = new SongDao();
 
@@ -25,7 +17,7 @@ console.log(song);
 // set content
 
 const display = document.getElementById("display");
-display.innerHTML = musician.instrument;
+display.innerHTML = song.voices[0] + " " + song.instruments[0];
 
 const content = document.getElementById("content");
 content.innerHTML = song.band + " - " + song.title;
