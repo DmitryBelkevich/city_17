65c7390664ba84c591dbd9a6aad0b7347c0af321
DmitryBelkevich <harleydav@mail.ru>
2026-08-18 23:59:15 +0300

    Update SongView.js

--- views/SongView.js
+++ views/SongView.js
@@ -10,8 +10,7 @@ export default class SongView {
   }
   
   setDisplay() {
-    console.log("set Display");
-    // const display = document.getElementById("display");
-    // display.innerHTML = this.#song.getVoice(0) + " " + this.#song.getInstrument(0);
+    const display = document.getElementById("display");
+    display.innerHTML = this.#song.getVoice(0) + " " + this.#song.getInstrument(0);
   }
 }
