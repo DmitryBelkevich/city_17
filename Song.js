84eed8cf6dce15f0ddbddf9d6a5d41e8bb78a2e3
DmitryBelkevich <harleydav@mail.ru>
2026-08-18 03:56:36 +0300

    Update Song.js

--- Song.js
+++ Song.js
@@ -1,9 +1,24 @@
 export default class Song {
+  #id;
+  #band;
+  #title;
+  #text;
+  #score;
+  #playback;
+  
   get id() {
     return this.id;
   }
   set id(id) {
     this.id = id;
+  }
+
+  get band() {
+    return this.band;
+  }
+
+  set band(band) {
+    this.band = band;
   }
 
   get title() {
