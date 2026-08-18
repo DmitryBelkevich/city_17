2458b9dd6b29516246c9c7589b70d2cecbcb4612
DmitryBelkevich <harleydav@mail.ru>
2026-08-18 04:25:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import Song from './Song.js';
 const song = new Song();
 
 song.id = 1;
-// song.band = "band1";
-// song.title = "title1";
-// song.text = "text1";
-// song.score = "score1";
-// song.playback = "playback1";
+song.band = "band1";
+song.title = "title1";
+song.text = "text1";
+song.score = "score1";
+song.playback = "playback1";
 
 window.song = song;
