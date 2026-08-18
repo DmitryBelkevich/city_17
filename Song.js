59a42e2d401b7da0ccf4a2068eb05f1662381563
DmitryBelkevich <harleydav@mail.ru>
2026-08-18 04:24:45 +0300

    Update Song.js

--- Song.js
+++ Song.js
@@ -1,10 +1,10 @@
 export default class Song {
   #id;
-  band;
-  title;
-  text;
-  score;
-  playback;
+  #band;
+  #title;
+  #text;
+  #score;
+  #playback;
   
   get id() {
     return this.#id;
@@ -12,5 +12,45 @@ export default class Song {
   
   set id(id) {
     this.#id = id;
+  }
+
+  get band() {
+    return this.#band;
+  }
+
+  set band(band) {
+    this.#band = band;
+  }
+
+  get title() {
+    return this.#title;
+  }
+
+  set title(title) {
+    this.#title = title;
+  }
+
+  get text() {
+    return this.#text;
+  }
+
+  set text(text) {
+    this.#text = text;
+  }
+
+  get score() {
+    return this.#score;
+  }
+
+  set score(score) {
+    this.#score = score;
+  }
+
+  get playback() {
+    return this.#playback;
+  }
+
+  set playback(playback) {
+    this.#playback = playback;
   }
 }
