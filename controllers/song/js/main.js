89c02d16d9ac722ecb0712606ff617db711c0354
DmitryBelkevich <harleydav@mail.ru>
2026-08-18 18:53:55 +0300

    Update main.js

--- controllers/song/js/main.js
+++ controllers/song/js/main.js
@@ -12,7 +12,7 @@ const songDao = new SongDao();
 
 const song = await songDao.getById(id);
 
-console.log("--- " + song.id + " ---");
+console.log("--- " + song.getVoice(1) + " ---");
 
 // set content
 
