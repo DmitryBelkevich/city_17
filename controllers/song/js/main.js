fe85ae70e6c952645ed5cdd4af4c73c27becf8b5
DmitryBelkevich <harleydav@mail.ru>
2026-08-18 19:31:52 +0300

    Update main.js

--- controllers/song/js/main.js
+++ controllers/song/js/main.js
@@ -14,6 +14,7 @@ async function main() {
   try {
     const song = await songDao.getById(id);
 
+    window.s = song;
     console.log(song);
   } catch (error) {
     console.error("Ошибка:", error);
