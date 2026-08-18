b47291dbfbd45d4247b8ed2ab18b21f5349123e4
DmitryBelkevich <harleydav@mail.ru>
2026-08-18 07:19:45 +0300

    Update Musician.js

--- models/Musician.js
+++ models/Musician.js
@@ -1,4 +1,20 @@
 export default class Musician {
   #voice;
   #instrument;
+
+  get voice() {
+    return this.#voice;
+  }
+  
+  set voice(voice) {
+    this.#voice = voice;
+  }
+
+  get instrument() {
+    return this.#instrument;
+  }
+  
+  set instrument(instrument) {
+    this.#instrument = instrument;
+  }
 }
