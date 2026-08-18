839ea3ca95aa386baab5e4126d39914d26b28a59
DmitryBelkevich <harleydav@mail.ru>
2026-08-18 03:29:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-import Song from './Song.js';
+// import Song from './Song.js';
 
-const song = new Song('Polina');
-console.log(song.sayHello());
+// const song = new Song('Polina');
+// console.log(song.sayHello());
 
 const user = {
     name: "Alex Rivera",
     role: "Developer",
     experience: "5 years"
 };
+
+console.log(user);
