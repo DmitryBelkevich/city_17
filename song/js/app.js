d41047783f5b5ce245e3d79fd210dc0bc0d5733a
DmitryBelkevich <harleydav@mail.ru>
2026-08-30 22:05:14 +0300

    Delete song/js/app.js

--- song/js/app.js
+++
@@ -1,8 +0,0 @@
-import FrontController from '../../controllers/FrontController.js';
-
-function main() {
-  const frontController = new FrontController();
-  frontController.dispatch("/song");
-}
-
-const app = main();
