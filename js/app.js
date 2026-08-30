7fc123f9d684b035e9a0c136132fd9c1f2eeedea
DmitryBelkevich <harleydav@mail.ru>
2026-08-30 21:12:54 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,3 @@
-class Dispatcher {
-  
-}
-
-class FrontController {
-  constructor() {
-    this.routes = {};
-
-    this.register('/', () => console.log('Home Page View'));
-    this.register('/list', () => console.log('List Page View'));
-    this.register('/song', () => console.log('Song Page View'));
-    this.register('/404', () => console.log('Page not found'));
-    
-    window.addEventListener('popstate', () => this.handleRoute());
-  }
-
-  register(path, action) {
-    this.routes[path] = action;
-  }
-  
-  dispatch(path) {
-    const action = this.routes[path] || this.routes['/404'];
-    action();
-  }
-
-  handleRoute() {
-    const path = window.location.pathname;
-    this.dispatch(path);
-  }
-}
-
 function main() {
   const frontController = new FrontController();
   frontController.dispatch("/");
