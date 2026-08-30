2e358fa15f960d48d2f31fb205fd49156474f6dc
DmitryBelkevich <harleydav@mail.ru>
2026-08-31 00:22:37 +0300

    Update Dispatcher.js

--- controllers/Dispatcher.js
+++ controllers/Dispatcher.js
@@ -5,6 +5,7 @@ class Song {
 class SongController {
   constructor(params) {
     const id = params.get("id");
+    console.log(id);
     
     this.song = new Song();//get from database by id
     // this.view = view;
