4fab939d56b8a49be1b0b07e47cede0bb681beec
DmitryBelkevich <harleydav@mail.ru>
2026-08-30 22:03:57 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@ import FrontController from '../controllers/FrontController.js';
 
 function main() {
   const frontController = new FrontController();
-
-  const path = window.location.pathname;
-  frontController.dispatch(path);
+  frontController.dispatch("/");
 }
 
 const app = main();
