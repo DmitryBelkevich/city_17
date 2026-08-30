58e030c9d2735d7d744c731640ae58f8a166d6cd
DmitryBelkevich <harleydav@mail.ru>
2026-08-30 19:58:24 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
 class FrontController {
-  dispatch(request) {
-    console.log("Page requested: " + request);
+  constructor() {
+    this.routes = {};
+    window.addEventListener('popstate', () => this.handleRoute());
+  }
+
+  register(path, action) {
+    this.routes[path] = action;
+  }
+  
+  dispatch(path) {
+    const action = this.routes[path] || this.routes['/404'];
+    action();
+  }
+
+  handleRoute() {
+    const path = window.location.pathname;
+    this.dispatch(path);
   }
 }
 
 function main() {
   const frontController = new FrontController();
   
-  frontController.dispatch("/song");
+  // frontController.dispatch("/song");
+  frontController.register('/', () => console.log('Home Page View'));
+  frontController.register('/about', () => console.log('About Page View'));
+  frontController.register('/404', () => console.log('Page Not Found'));
 }
 
 const app = main();
