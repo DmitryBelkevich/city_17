54fd24b87149cf578656f4bf78b9dcc6a9919199
DmitryBelkevich <harleydav@mail.ru>
2026-08-31 00:51:55 +0300

    Update SongController.js

--- controllers/SongController.js
+++ controllers/SongController.js
@@ -4,7 +4,6 @@ import SongView from '../views/SongView.js';
 export default class SongController {
   constructor(params) {
     const id = params.get("id");
-    console.log(id);
     
     this.song = new Song();//get from database by id
     this.view = new SongView();
