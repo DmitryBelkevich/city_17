3f6499c2a7aa455095367722c82d1ab375ae4082
DmitryBelkevich <harleydav@mail.ru>
2026-08-30 21:28:19 +0300

    Update FrontController.js

--- controllers/FrontController.js
+++ controllers/FrontController.js
@@ -1,4 +1,4 @@
-export default class FrontController {
+class Dispatcher {
   constructor() {
     this.routes = {};
 
@@ -6,21 +6,31 @@ export default class FrontController {
     this.register('/list', () => console.log('List Page View'));
     this.register('/song', () => console.log('Song Page View'));
     this.register('/404', () => console.log('Page not found'));
-    
+
     window.addEventListener('popstate', () => this.handleRoute());
   }
 
   register(path, action) {
     this.routes[path] = action;
   }
-  
-  dispatch(path) {
-    const action = this.routes[path] || this.routes['/404'];
-    action();
-  }
 
   handleRoute() {
     const path = window.location.pathname;
     this.dispatch(path);
+  }
+
+  dispatch(path) {
+    const action = this.routes[path] || this.routes['/404'];
+    action();
+  }
+}
+
+export default class FrontController {
+  constructor() {
+    this.dispatcher = new Dispatcher();
+  }
+  
+  dispatch(path) {
+    this.dispatcher.dispatch(path);
   }
 }
