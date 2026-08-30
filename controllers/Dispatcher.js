9eb224fb044b5d85dab366abb2b7b42fc4122989
DmitryBelkevich <harleydav@mail.ru>
2026-08-30 21:35:17 +0300

    Create Dispatcher.js

---
+++ controllers/Dispatcher.js
@@ -0,0 +1,26 @@
+export default class Dispatcher {
+  constructor() {
+    this.routes = {};
+
+    this.register('/', () => console.log('Main Page View'));
+    this.register('/list', () => console.log('List Page View'));
+    this.register('/song', () => console.log('Song Page View'));
+    this.register('/404', () => console.log('Page not found'));
+
+    window.addEventListener('popstate', () => this.handleRoute());
+  }
+
+  register(path, action) {
+    this.routes[path] = action;
+  }
+
+  handleRoute() {
+    const path = window.location.pathname;
+    this.dispatch(path);
+  }
+
+  dispatch(path) {
+    const action = this.routes[path] || this.routes['/404'];
+    action();
+  }
+}
