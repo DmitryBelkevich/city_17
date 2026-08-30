438973b1b9e4ab70be311eec8f52ed2c47bed805
DmitryBelkevich <harleydav@mail.ru>
2026-08-30 23:54:55 +0300

    Update Dispatcher.js

--- controllers/Dispatcher.js
+++ controllers/Dispatcher.js
@@ -2,10 +2,24 @@ export default class Dispatcher {
   constructor() {
     this.routes = {};
     
-    this.register("", () => console.log('Main Page View'));
-    this.register("/list", () => console.log('List Page View'));
-    this.register("/song", () => console.log('Song Page View'));
-    this.register("/404", () => console.log('Page not found'));
+    this.register("", () => {
+      console.log('Main Page View')
+      console.log('Load MainController')
+    });
+    
+    this.register("/list", () => {
+      console.log('List Page View')
+      console.log('Load ListController')
+    });
+    
+    this.register("/song", () => {
+      console.log('Song Page View')
+      console.log('Load SongController')
+    });
+    
+    this.register("/404", () => {
+      console.log('Page not found')
+    });
     
     window.addEventListener('popstate', () => this.handleRoute());
   }
