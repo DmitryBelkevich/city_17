20fab44c6c275f74c1cd975335e1b1f3a78c8be1
DmitryBelkevich <harleydav@mail.ru>
2026-08-31 08:24:36 +0300

    Update SongDao.js

--- dao/SongDao.js
+++ dao/SongDao.js
@@ -2,9 +2,11 @@ import Song from '../models/Song.js';
 import DatabaseLoader from './loaders/DatabaseLoader.js';
 
 export default class SongDao {
+  constructor() {
+    this.databaseLoader = new DatabaseLoader();
+  }
+  
   getById(id) {
-    new DatabaseLoader();
-    
     console.log("get from database by id=" + id);
 
     const song = new Song();
