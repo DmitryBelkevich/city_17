9a9b87617a65ce70f13791f5a417f13450925c55
DmitryBelkevich <harleydav@mail.ru>
2026-08-31 07:46:45 +0300

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,10 +1,13 @@
 import FrontController from '../controllers/front/FrontController.js';
+import DatabaseLoader from '../dao/loaders/DatabaseLoader.js';
 
 function main() {
   const frontController = new FrontController();
 
   const path = window.location.pathname;
   frontController.dispatch(path);
+
+  const databaseLoader = new DatabaseLoader();
 }
 
 const app = main();
