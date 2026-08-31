5e9ce4367be1aca1e20aba0203821f79a73e6665
DmitryBelkevich <harleydav@mail.ru>
2026-08-31 07:58:10 +0300

    Update DatabaseLoader.js

--- dao/loaders/DatabaseLoader.js
+++ dao/loaders/DatabaseLoader.js
@@ -2,7 +2,7 @@ export default class DatabaseLoader {
   // static path = '../../database/songs.json';
   
   async loadData() {
-    const path = '../../database/songs.json';
+    const path = '../database/songs.json';
     
     try {
       // 1. Wait for the server headers and response status
