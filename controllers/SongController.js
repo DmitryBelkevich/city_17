732460f8e4f3627bee17e05fb1cbba77f0f5c27c
DmitryBelkevich <harleydav@mail.ru>
2026-08-31 10:36:41 +0300

    Update SongController.js

--- controllers/SongController.js
+++ controllers/SongController.js
@@ -3,17 +3,27 @@ import SongView from '../views/SongView.js';
 import SongService from '../services/SongService.js';
 
 export default class SongController {
-  constructor(params) {
-    const id = params.get("id");
-
+  #params;
+  
+  constructor() {
     // model
-    const songService = new SongService();
-    this.song = songService.getById(id);
-
-    console.log(this.song);
+    this.songService = new SongService();
 
     // view
     this.view = new SongView();
+  }
+
+  set params(params) {
+    this.#params = params;
+  }
+
+  async init() {
+    // model
+    const song = await this.songService.getById(this.params.get("id"));
+
+    console.log(song);
+
+    // view
     this.view.update();
   }
 }
