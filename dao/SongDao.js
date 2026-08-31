9cd474ea1d088dfa5bb98325f71274f4ec66b24a
DmitryBelkevich <harleydav@mail.ru>
2026-08-31 08:33:07 +0300

    Update SongDao.js

--- dao/SongDao.js
+++ dao/SongDao.js
@@ -7,7 +7,6 @@ export default class SongDao {
   }
   
   async getById(id) {
-    console.log("get from database by id=" + id);
     const data = await this.databaseLoader.loadData();
     console.log(data);
 
