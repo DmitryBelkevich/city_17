56ea2ca2d8b170b33b164f7fa82eed2ce144184c
DmitryBelkevich <harleydav@mail.ru>
2026-08-31 10:30:07 +0300

    Update Dispatcher.js

--- controllers/front/Dispatcher.js
+++ controllers/front/Dispatcher.js
@@ -6,34 +6,23 @@ export default class Dispatcher {
   constructor() {
     this.routes = {};
     
-    this.register("", () => new MainController());
-    this.register("/list", () => new ListController());
-    this.register("/song", (params) => new SongController(params));
-    
-    this.register("/404", () => {
-      console.log('Page not found');
-    });
-    
-    window.addEventListener('popstate', () => this.handleRoute());
+    this.addRoute("", new MainController());
+    this.addRoute("/list", new ListController());
+    this.addRoute("/song", new SongController());
   }
 
-  register(path, action) {
-    this.routes[path] = action;
-  }
-
-  handleRoute() {
-    const path = window.location.pathname;
-    this.dispatch(path);
+  addRoute(path, controller) {
+    this.routes[path] = controller;
   }
 
   dispatch(path) {
     const appName = "city_17";
     const routeName = path.slice(("/" + appName).length, -1);
-
-    const params = this.getParams();
     
-    const action = this.routes[routeName] || this.routes["/404"];
-    action(params);
+    const controller = this.routes[routeName];
+    
+    controller.params = this.getParams();
+    controller.init();
   }
 
   getParams() {
