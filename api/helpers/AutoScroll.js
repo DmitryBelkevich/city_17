fa1ae80410a9c1497498a77689e88c4bba907251
DmitryBelkevich <harleydav@mail.ru>
2026-09-02 00:18:59 +0300

    Update and rename Autoscroll.js to AutoScroll.js

---
+++ api/helpers/AutoScroll.js
@@ -0,0 +1,26 @@
+export default class AutoScroll {
+  #state = false;
+  
+  set state(state) {
+    this.#state = state;
+  }
+
+  nextState() {
+    this.#state = !this.#state;
+  }
+
+  run() {
+    // this.nextState();
+    this.operation();
+  }
+
+  operation() {
+    // Прокручиваем страницу на 1 пиксель вниз
+    window.scrollBy(0, 1); 
+    
+    // Проверяем, не достигли ли мы конца страницы
+    if ((window.innerHeight + window.scrollY) < document.body.offsetHeight) {
+      requestAnimationFrame(autoScroll);
+    }
+  }
+}
