a1c83549b40616cfc88e32b1d65ffcec0f98d00a
DmitryBelkevich <harleydav@mail.ru>
2026-09-02 01:06:49 +0300

    Update AutoScroll.js

--- api/helpers/AutoScroll.js
+++ api/helpers/AutoScroll.js
@@ -15,13 +15,12 @@ export default class AutoScroll {
   }
 
   operation() {
-    function autoScroll() {
+    function autoScroll() {console.log(this);
       // Прокручиваем страницу на 1 пиксель вниз
       window.scrollBy(0, 1); 
       
       // Проверяем, не достигли ли мы конца страницы
       if ((window.innerHeight + window.scrollY) < document.body.offsetHeight && true) {
-        console.log(!super.#state);
         requestAnimationFrame(autoScroll);
       }
 
