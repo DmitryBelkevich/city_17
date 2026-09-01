3b7d1b5bbcd48ee81e01c395636f6eaeb3da8d28
DmitryBelkevich <harleydav@mail.ru>
2026-09-02 02:55:04 +0300

    Update AutoScroll.js

--- api/helpers/AutoScroll.js
+++ api/helpers/AutoScroll.js
@@ -1,6 +1,6 @@
 export default class AutoScroll {
   operation() {
-    var speed = 10;//1 slow, 10 normal, 100 fast.
+    var speed = 50;//1 slow, 10 normal, 100 fast.
     
     let animationFrameId;
 
