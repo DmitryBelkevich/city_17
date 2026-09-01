f7722f923b8a682145e3730b3941a9a6b514e503
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 06:22:57 +0300

    Update SongView.js

--- api/views/SongView.js
+++ api/views/SongView.js
@@ -1,9 +1,15 @@
 export default class SongView {
   constructor() {
+    this.body = document.body;
     
+    this.title = document.createElement("div");
+    this.title.id = "title";
+
+    this.body.append(this.title);
   }
 
   update() {
     console.log("draw SongView");
+    this.title.textContent = "Hello World";
   }
 }
