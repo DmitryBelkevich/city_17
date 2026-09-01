32960c5d479fe5db1c09a09caf15494c5a3bda6b
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 16:42:16 +0300

    Update SongView.js

--- api/views/SongView.js
+++ api/views/SongView.js
@@ -36,6 +36,11 @@ export default class SongView {
     this.text.id = "text";
     this.loadCSS("../css/song/text.css");
 
+    // Footer
+    this.footer = document.createElement("div");
+    this.footer.id = "footer";
+    this.loadCSS("../css/song/footer.css");
+
     // body fill
     this.body = document.body;
     this.body.append(this.title, this.tabs, this.text);
