92a16637ad2663b1f62c6aedb0ed28178043fc4a
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 05:40:53 +0300

    Update SongDao.js

--- api/dao/SongDao.js
+++ api/dao/SongDao.js
@@ -7,8 +7,8 @@ export default class SongDao {
   }
   
   async getById(id) {
-    const path = "../database/songs.json";
-    const data = await this.databaseLoader.loadData(path);
+    // const path = "../database/songs.json";
+    const data = await this.databaseLoader.loadData("../database/songs.json");
     const result = data.find(song => song.id == id) || null;
 
     if (!result)
