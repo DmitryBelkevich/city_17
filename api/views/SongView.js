ba769f60c28e8018e346558d23e61396a776841a
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 22:25:30 +0300

    Update SongView.js

--- api/views/SongView.js
+++ api/views/SongView.js
@@ -43,7 +43,7 @@ export default class SongView {
     // Auto-scroll
     this.autoscroll = document.createElement("button");
     this.autoscroll.id = "autoscroll";
-    this.autoscroll.textContent = "⏬";
+    this.autoscroll.textContent = "⏬ auto-scroll";
 
     this.settings.append(this.autoscroll);
 
