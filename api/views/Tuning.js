5f8ebb731686c4808239d40b0b795c013a68638c
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 18:48:55 +0300

    Create Tuning.js

---
+++ api/views/Tuning.js
@@ -0,0 +1,22 @@
+export default class Tuning {
+  static isStandard(tuning) {
+    const isEqual = (a, b) => a.length === b.length && a.every((val, i) => val === b[i]);
+    
+    // *** Guitars ***
+    
+    if (isEqual(tuning, ["E", "A", "D", "G", "B", "E"]))
+      return true;
+
+    // *** Bass Guitars ***
+    
+    if (isEqual(tuning, ["E", "A", "D", "G"]))
+      return true;
+
+    // *** 5-strings Bass Guitars ***
+    
+    if (isEqual(tuning, ["B", "E", "A", "D", "G"]))
+      return true;
+    
+    return false;
+  }
+}
