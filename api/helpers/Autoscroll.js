459340f6873106196b250978f87c346cf3561138
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 23:20:35 +0300

    Update Autoscroll.js

--- api/helpers/Autoscroll.js
+++ api/helpers/Autoscroll.js
@@ -1,3 +1,13 @@
 export default class Autoscroll {
+  constructor() {
+    this.state = false;
+  }
   
+  set state(state) {
+    this.state = state;
+  }
+
+  run() {
+    console.log("Autoscroll");
+  }
 }
