aa8554c2d51c49764d9bdf7f84ea28418a7ab15a
DmitryBelkevich <harleydav@mail.ru>
2026-09-02 00:37:44 +0300

    Update AutoScroll.js

--- api/helpers/AutoScroll.js
+++ api/helpers/AutoScroll.js
@@ -10,7 +10,7 @@ export default class AutoScroll {
   }
 
   run() {
-    // this.nextState();
+    this.nextState();
     this.operation();
   }
 
@@ -20,7 +20,7 @@ export default class AutoScroll {
       window.scrollBy(0, 1); 
       
       // Проверяем, не достигли ли мы конца страницы
-      if ((window.innerHeight + window.scrollY) < document.body.offsetHeight) {
+      if ((window.innerHeight + window.scrollY) < document.body.offsetHeight || !this.#state) {
         requestAnimationFrame(autoScroll);
       }
     }
