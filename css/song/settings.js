140a4dcc35705db907b823f26b2861b607a7dd8d
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 21:18:14 +0300

    Create settings.js

---
+++ css/song/settings.js
@@ -0,0 +1,22 @@
+#settings {
+  margin: 0px;
+  padding: 10px;
+  
+  border: 1px solid black;
+}
+
+#settings {
+  button {
+    margin: 0px;
+    padding: 10px;
+  }
+
+  select {
+    margin: 0px;
+    padding: 10px;
+  }
+}
+
+#settings {
+  /* display: none; */
+}
