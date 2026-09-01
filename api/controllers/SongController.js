8b0392609617a24e767f657e4ceccb78318898d1
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 20:14:05 +0300

    Update SongController.js

--- api/controllers/SongController.js
+++ api/controllers/SongController.js
@@ -31,7 +31,7 @@ export default class SongController {
     this.view.setTabs(this.song.score, this.song.playback);
 
     this.song.instruments.forEach((instrument, index) => {
-      this.view.addTuning(instrument.title, instrument.tuning, instrument.capo1);
+      this.view.addTuning(instrument.title, instrument.tuning, instrument.capo);
     });
     
     const text = await this.loader.loadData(this.song.text);
