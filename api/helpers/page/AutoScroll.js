12945c4cbd5c2b2dd6e23970e3021e4139c1edcd
DmitryBelkevich <harleydav@mail.ru>
2026-09-05 06:33:50 +0300

    Update AutoScroll.js

--- api/helpers/page/AutoScroll.js
+++ api/helpers/page/AutoScroll.js
@@ -1,31 +1,23 @@
 export default class AutoScroll {
-  constructor() {
-    this.speed = 0;//1 slow, 10 normal, 100 fast.
+  animationFrameId;
+
+  step() {
+    window.scrollBy(0, 1);
+    
+    if ((window.innerHeight + window.scrollY) < document.body.offsetHeight) {
+      setTimeout(() => {
+        animationFrameId = requestAnimationFrame(this.step);
+      }, 1000 / 15);
+    }
   }
   
+  // 1. Start the scroll loop
   run() {
-    let animationFrameId;
-
-    const step = () => {
-      window.scrollBy(0, 1);
-      
-      if ((window.innerHeight + window.scrollY) < document.body.offsetHeight) {
-        setTimeout(() => {
-          animationFrameId = requestAnimationFrame(step);
-        }, (1000 / 0));
-      }
-    }
-    
-    // 1. Start the scroll loop
-    animationFrameId = requestAnimationFrame(step);
-    
-    // 2. Stop the scroll loop
-    function stopAutoScroll() {
-      cancelAnimationFrame(animationFrameId);
-    }
+    animationFrameId = requestAnimationFrame(this.step);
   }
-
-  speedUp() {
-    this.speed = 15;
+  
+  // 2. Stop the scroll loop
+  stopAutoScroll() {
+    cancelAnimationFrame(animationFrameId);
   }
 }
