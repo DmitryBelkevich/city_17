cecab8fd331e00b39c789fd2cbc5faf36a49c77a
DmitryBelkevich <harleydav@mail.ru>
2026-09-05 05:51:24 +0300

    Update AutoScroll.js

--- api/helpers/page/AutoScroll.js
+++ api/helpers/page/AutoScroll.js
@@ -1,10 +1,10 @@
 export default class AutoScroll {
-  speed = 15;//1 slow, 10 normal, 100 fast.
+  speed = 15*5;//1 slow, 10 normal, 100 fast.
   
   operation() {
     let animationFrameId;
 
-    function step() {
+    const step = function() {
       window.scrollBy(0, 1);
       
       if ((window.innerHeight + window.scrollY) < document.body.offsetHeight) {
