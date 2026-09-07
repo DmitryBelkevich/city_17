61363de6dcc38069eee0fc30fdb8ca76a2db332c
DmitryBelkevich <harleydav@mail.ru>
2026-09-07 14:00:24 +0300

    Update ListView.js

--- api/views/ListView.js
+++ api/views/ListView.js
@@ -9,7 +9,7 @@ export default class ListView {
 
   addSong(song) {
     const song_e = document.createElement("div");
-    song_e.textContent = song.band + " " + song.title;
+    song_e.textContent = song.band + " - " + song.title;
 
     this.body.append(song_e);
   }
