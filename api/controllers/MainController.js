8d19bd24a38483031f7b1557b65888d31575b299
DmitryBelkevich <harleydav@mail.ru>
2026-09-07 13:04:10 +0300

    Update MainController.js

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -1,13 +1,7 @@
 import MainView from '../views/MainView.js';
 
 export default class MainController {
-  constructor() {
-    console.log("MainController constructor");
-  }
-  
   async init() {
-    console.log("MainController init");
-    
     // view
     this.view = new MainView();
     this.view.update();
