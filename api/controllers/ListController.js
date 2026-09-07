c3c71a5b3cbe93f1f35bc753b9c8fee88010144f
DmitryBelkevich <harleydav@mail.ru>
2026-09-07 13:52:29 +0300

    Update ListController.js

--- api/controllers/ListController.js
+++ api/controllers/ListController.js
@@ -6,12 +6,12 @@ export default class ListController {
     // model
     this.songService = new SongService();
     this.songs = await this.songService.getAll();
-
-    this.songs.forEach((song, index) => {
-      console.log(song);
-    });
     
     // view
     this.view = new ListView();
+
+    this.songs.forEach((song, index) => {
+      this.view.addSong(song);
+    });
   }
 }
