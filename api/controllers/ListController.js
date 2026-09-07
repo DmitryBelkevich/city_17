a653a4b599936572d593128e65ad5bc491144c31
DmitryBelkevich <harleydav@mail.ru>
2026-09-07 12:48:00 +0300

    Update ListController.js

--- api/controllers/ListController.js
+++ api/controllers/ListController.js
@@ -2,6 +2,8 @@ import ListView from '../views/ListView.js';
 
 export default class ListController {
   async init() {
+    console.log("ListController init");
+    
     // view
     this.view = new ListView();
     this.view.update();
