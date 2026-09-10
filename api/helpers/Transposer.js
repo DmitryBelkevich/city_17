6e580c74450d1e626f7f6069b7d2c0fac74ed0ab
DmitryBelkevich <harleydav@mail.ru>
2026-09-11 01:54:48 +0300

    Update Transposer.js

--- api/helpers/Transposer.js
+++ api/helpers/Transposer.js
@@ -1,4 +1,10 @@
 export default class Transposer {
+  #key;
+  
+  set key(key) {
+    this.#key = key;
+  }
+  
   transposeUp() {
     const chords_lines = document.querySelectorAll('div.chords');
     
@@ -11,6 +17,13 @@ export default class Transposer {
   }
 
   transposeDown() {
-    console.log("Transpose down");
+    const chords_lines = document.querySelectorAll('div.chords');
+    
+    chords_lines.forEach(chords_line => {
+      const chords_line_arr = chords_line.querySelectorAll('div');
+      chords_line_arr.forEach((chord_div) => {
+        chord_div.innerHTML -= "-";
+      });
+    });
   }
 }
