aef69f1b95fd9b5129a0597d3399d4ffaa4e2942
DmitryBelkevich <harleydav@mail.ru>
2026-09-11 02:38:45 +0300

    Update Transposer.js

--- api/helpers/Transposer.js
+++ api/helpers/Transposer.js
@@ -1,5 +1,19 @@
 export default class Transposer {
   #key;
+  #chords = [];
+
+  constructor() {
+    const chords_lines = document.querySelectorAll('div.chords');
+    
+    chords_lines.forEach(chords_line => {
+      const chords_line_arr = chords_line.querySelectorAll('div');
+        chords_line_arr.forEach((chord_div) => {
+        this.#chords.push(chord_div);
+      });
+    });
+
+    console.log(this.#chords);
+  }
   
   set key(key) {
     this.#key = key;
