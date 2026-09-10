9304ee562180837a4020f682897e03c5fefc64ed
DmitryBelkevich <harleydav@mail.ru>
2026-09-10 17:03:43 +0300

    Update SongService.js

--- api/services/SongService.js
+++ api/services/SongService.js
@@ -19,10 +19,11 @@ export default class SongService {
     song.text_content = await this.loader.loadData(song.text_json);
 
     const marks = song.text_content.filter((element) => {
-      element.class.startWith("mark");
+      // element.class.startWith("mark");
+      console.log(element.class);
     });
 
-    console.log(marks);
+    // console.log(marks);
 
     song.instruments.forEach((instrument, index) => {
       if (instrument.title == "Guitar")
