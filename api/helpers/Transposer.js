5ea94de1ea29c67d26abdfd7c50df8f14865d4d1
DmitryBelkevich <harleydav@mail.ru>
2026-09-11 03:33:14 +0300

    Update Transposer.js

--- api/helpers/Transposer.js
+++ api/helpers/Transposer.js
@@ -49,7 +49,7 @@ export default class Transposer {
     console.log(chord == "Gm");
     
     if (chord == "Gm")
-      chord = "000";
+      chord = "G#m";
 
     if (chord == "G#m")
       chord = "Am";
