9eabe70e183fcf3298507fb77517e0ac6479f991
DmitryBelkevich <harleydav@mail.ru>
2026-09-11 03:05:55 +0300

    Update Transposer.js

--- api/helpers/Transposer.js
+++ api/helpers/Transposer.js
@@ -21,7 +21,7 @@ export default class Transposer {
 
     // transpose key on display
     const key_e = document.getElementById("key");
-    key_e.textContent = this.#key;
+    // key_e.textContent = this.#key;
 
     // transpose all chords
     this.#chords.forEach((element) => {
@@ -35,11 +35,12 @@ export default class Transposer {
 
     // transpose key on display
     const key_e = document.getElementById("key");
-    key_e.textContent = this.#key;
+    // key_e.textContent = this.#key;
 
     // transpose all chords
     this.#chords.forEach((element) => {
-      console.log(element);
+      const chord = element.innerHTML;
+      this.downChord(chord);
     });
   }
 
