55b8e0fed58d85ee9103d78e2856ede279e6c017
DmitryBelkevich <harleydav@mail.ru>
2026-09-11 05:16:19 +0300

    Update SongController.js

--- api/controllers/SongController.js
+++ api/controllers/SongController.js
@@ -23,7 +23,10 @@ export default class SongController {
     
     this.view.setPageTitle(this.song.band, this.song.title);
     this.view.setTitle(this.song.band, this.song.title);
-    this.view.setKey(this.song.key);
+    
+    if (this.song.key != undefined)
+      this.view.setKey(this.song.key);
+    
     this.song.instruments.forEach((instrument, index) => {
       this.view.addTuning(instrument.title, instrument.tuning, instrument.capo);
     });
